b94aff05cc5118ae539afe014149fa4969d065e9
HuanaVu <huanvu210@gmail.com>
2025-03-31 22:26:33 +0700

    31

--- app/src/main/java/com/sinhvien/appcalories/ProfileActivity.java
+++ app/src/main/java/com/sinhvien/appcalories/ProfileActivity.java
@@ -15,7 +15,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.sinhvien.appcalories.models.UserProfile;
 
-public class ProfileActivity extends AppCompatActivity {
+public class  ProfileActivity extends AppCompatActivity {
 
     private EditText edtWeight, edtHeight;
     private Spinner spnGender, spnGoal;
